897418e54825af1ebebd8692c6076edc1544dfbf
Diego Miguel <dml1001@alu.ubu.es>
2020-06-30 05:27:07 +0900

    Implemented final UI for 'InsertShips'

--- app/src/main/java/com/diego/hanbackbattleship/InsertShips.java
+++ app/src/main/java/com/diego/hanbackbattleship/InsertShips.java
@@ -3,10 +3,12 @@ package com.diego.hanbackbattleship;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.diego.hanbackbattleship.control.Player;
@@ -26,16 +28,30 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
 
     private OceanPrinter playerOceanPrinter;
 
+    Typeface exxaGameTypeFace;
+    Typeface gameCubeTypeFace;
+
+    private boolean placeShipButtonEnabled = false;
+    private boolean rotateButtonEnabled = false;
+    private boolean nextButtonEnabled = false;
+
+    private ImageView cornersLeft;
+    private ImageView cornersRight;
     private TextView shipName;
-    private Button okButton;
-    private Button rotateButton;
+    private ImageView placeShipButton;
+    private ImageView rotateButton;
+    private ImageView nextButton;
 
     private int[] lastCoordsInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_insert_ships);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         player = new Player();
         Player opponent = new Player();
@@ -50,15 +66,42 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
         shipName = findViewById(R.id.ship_type);
         shipName.setText(shipTypes[shipCounter].toString());
 
-        okButton = findViewById(R.id.ok_button);
+        cornersLeft = findViewById(R.id.corners_left);
+        cornersRight = findViewById(R.id.corners_right);
+        placeShipButton = findViewById(R.id.place_ship_button);
+        placeShipButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onPlaceShipClicked(v);
+            }
+        });
         rotateButton = findViewById(R.id.rotate_button);
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onRotateClicked(v);
+            }
+        });
+        nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onNextClicked(v);
+            }
+        });
+
+        TextView placeShipText = findViewById(R.id.place_ship_text);
+
+        // set fonts
+        exxaGameTypeFace = Typeface.createFromAsset(getAssets(), "fonts/exxa_game.ttf");
+        gameCubeTypeFace = Typeface.createFromAsset(getAssets(), "fonts/game_cube.ttf");
+
+        placeShipText.setTypeface(exxaGameTypeFace);
+        shipName.setTypeface(gameCubeTypeFace);
 
         playerOceanPrinter.printOceanWhenAddingShips();
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-        super.onResume();
+        super.onWindowFocusChanged(hasFocus);
 
         View quarterUpperLeft = findViewById(R.id.quarter_1);
         View quarterUpperRight = findViewById(R.id.quarter_2);
@@ -71,17 +114,28 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
         quarterLowerRight.setOnClickListener(this);
 
         playerOceanPrinter.placeQuarters(currentQuarter);
+        shipName.animate().alpha(1f).setDuration(OceanPrinter.SHORT_DURATION);
+        cornersLeft.animate().alpha(0.3f).setDuration(OceanPrinter.SHORT_DURATION);
+        cornersRight.animate().alpha(0.3f).setDuration(OceanPrinter.SHORT_DURATION);
     }
 
-    public void onOkClicked(View view) {
-        shipCounter++;
-        if (shipCounter < ShipType.values().length ) {
-            shipName.setText(shipTypes[shipCounter].toString());
-            okButton.setEnabled(false);
-            rotateButton.setEnabled(false);
-            orientation = Orientation.HORIZONTAL;
-            playerOceanPrinter.printOceanWhenAddingShips(shipTypes[shipCounter]);
-        } else {
+
+    public void onPlaceShipClicked(View view) {
+        if (placeShipButtonEnabled) {
+            shipCounter++;
+            if (shipCounter < ShipType.values().length ) {
+                nextShipAnimation();
+                orientation = Orientation.HORIZONTAL;
+                playerOceanPrinter.printOceanWhenAddingShips(shipTypes[shipCounter]);
+            }
+            if (shipCounter == ShipType.values().length) {
+                donePlacingShipsAnimation();
+            }
+        }
+    }
+
+    public void onNextClicked(View view) {
+        if (nextButtonEnabled) {
             Intent intent = new Intent(this, BattleActivity.class);
             DataHolder.getInstance().save(BattleActivity.ID_OCEAN, player.getOcean());
             DataHolder.getInstance().save(BattleActivity.ID_OPPONENT, player.getOpponent());
@@ -89,9 +143,47 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
         }
     }
 
+    private void donePlacingShipsAnimation() {
+        placeShipButtonEnabled = false;
+        rotateButtonEnabled = false;
+        rotateButton.animate().alpha(0f).setDuration(OceanPrinter.SHORT_DURATION);
+        placeShipButton.animate().alpha(0f).setDuration(OceanPrinter.SHORT_DURATION);
+        placeShipButton.animate().xBy(-53f).setDuration(OceanPrinter.SHORT_DURATION);
+        placeShipButton.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                placeShipButton.animate().xBy(53f).setDuration(OceanPrinter.SHORT_DURATION);
+            }
+        }, OceanPrinter.SHORT_DURATION);
+        nextButton.animate().alpha(1f).setDuration(OceanPrinter.SHORT_DURATION);
+        nextButton.animate().xBy(-53f).setDuration(OceanPrinter.SHORT_DURATION);
+        nextButtonEnabled = true;
+    }
+
+    private void nextShipAnimation() {
+        shipName.animate().alpha(0f).setDuration(OceanPrinter.SHORT_DURATION);
+        cornersLeft.animate().alpha(0f).setDuration(OceanPrinter.SHORT_DURATION);
+        cornersRight.animate().alpha(0f).setDuration(OceanPrinter.SHORT_DURATION);
+        shipName.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                shipName.setText(shipTypes[shipCounter].toString());
+                shipName.animate().alpha(1f).setDuration(OceanPrinter.SHORT_DURATION);
+                cornersLeft.animate().alpha(0.3f).setDuration(OceanPrinter.SHORT_DURATION);
+                cornersRight.animate().alpha(0.3f).setDuration(OceanPrinter.SHORT_DURATION);
+            }
+        }, OceanPrinter.SHORT_DURATION);
+        placeShipButtonEnabled = false;
+        rotateButtonEnabled = false;
+        placeShipButton.animate().alpha(0.5f).setDuration(OceanPrinter.SHORT_DURATION);
+        rotateButton.animate().alpha(0.5f).setDuration(OceanPrinter.SHORT_DURATION);
+    }
+
     public void onRotateClicked(View view) {
-        orientation = getOppositeOrientation(orientation);
-        addShip(lastCoordsInput, orientation);
+        if (rotateButtonEnabled) {
+            orientation = getOppositeOrientation(orientation);
+            addShip(lastCoordsInput, orientation);
+        }
     }
 
     @Override
@@ -99,7 +191,8 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
         int[] coords = KeyToCoordinateTranslator.translate(keyCode, currentQuarter);
         lastCoordsInput = coords;
         addShip(coords, orientation);
-        rotateButton.setEnabled(true);
+        rotateButton.animate().alpha(1f).setDuration(OceanPrinter.SHORT_DURATION);
+        rotateButtonEnabled = true;
         return true;
     }
 
@@ -108,18 +201,14 @@ public class InsertShips extends AppCompatActivity implements View.OnClickListen
     }
 
     public void addShip(int[] coords, Orientation orientation) {
-        TextView alert = findViewById(R.id.already_boat_alert);
         boolean shipWasAdded = player.addShip(shipTypes[shipCounter], coords[0], coords[1], orientation);
-        okButton.setEnabled(shipWasAdded);
+        placeShipButtonEnabled = shipWasAdded;
+        float alpha = shipWasAdded ? 1f : 0.5f;
+        placeShipButton.animate().alpha(alpha).setDuration(OceanPrinter.SHORT_DURATION);
         if (shipWasAdded) {
-            alert.setVisibility(View.GONE);
             playerOceanPrinter.printOceanWhenAddingShips(shipTypes[shipCounter]);
-            if (shipCounter == ShipType.values().length - 1) {
-                okButton.setText(R.string.next);
-            }
         } else {
             playerOceanPrinter.moveShipToIllegalCoords(shipTypes[shipCounter], coords, orientation);
-            alert.setVisibility(View.VISIBLE);
         }
     }
 
